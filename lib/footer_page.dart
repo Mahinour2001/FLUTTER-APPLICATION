2bd6eeb3121e7edd11c14dc9583bc6356ad52cef
Mina Ashraf <minachraf@outlook.com>
2021-09-04 16:53:03 +0200

    Gitignore config files

--- lib/footer_page.dart
+++ lib/footer_page.dart
@@ -3,24 +3,22 @@ import 'package:flutter/material.dart';
 class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
+    return BottomNavigationBar(
         currentIndex: 0, // this will be set when a new tab is tapped
         items: [
           BottomNavigationBarItem(
             icon: new Icon(Icons.home),
-            title: new Text('Home'),
+            label: 'Home',
           ),
           BottomNavigationBarItem(
             icon: new Icon(Icons.mail),
-            title: new Text('Messages'),
+            label: 'Messages',
           ),
           BottomNavigationBarItem(
               icon: Icon(Icons.person),
-              title: Text('Profile')
+              label:'Profile'
           )
         ],
-      ),
-    );
+      );
   }
 }
--- lib/screen.dart
+++ lib/screen.dart
@@ -1,40 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'footer_page.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
           title: Text(
-              'Sin Up',
+              'Sign Up',
               style: TextStyle(fontSize: 32),
           ),
         ),
         body: Center(
-        child: Text('WHERE ARTICLES EXIST'),
+          child: Text('WHERE ARTICLES EXIST'),
         ),
 
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0, // this will be set when a new tab is tapped in navigation
-          items: [
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.home),
-              title: new Text('Home'),
-              backgroundColor: Colors.pink,
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.mail),
-              title: new Text('Messages'),
-              backgroundColor: Colors.black,
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Text('Profile'),
-              backgroundColor: Colors.black,
-
-            )
-          ],
-        ),
+        bottomNavigationBar: Footer(),
       );
     }
   }
